759549ca4cc59452afd151b30cf20171e07d5419
MrYar0s <70350421+MrYar0s@users.noreply.github.com>
2020-10-09 22:44:57 +0300

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,253 @@
+#include "stack.h"
+
+void StackCreate(Stack* tack){
+    assert(tack);
+    VERIFY;
+
+    tack->data = (element*) calloc(START * sizeof(element) + sizeof(CANARY_1) + sizeof(CANARY_2), sizeof(element));
+
+    for(size_t i = 0; i < START; i++)
+        *(tack->data + sizeof(CANARY_1) + sizeof(element) * i) = POISON;
+
+    *(tack->data) = CANARY_1;
+    *(tack->data + sizeof(CANARY_1) + START * sizeof(element)) = CANARY_2;
+
+    tack->minlen = 0;
+    tack->maxlen = START;
+    tack->len = 0;
+    tack->hash = 0;
+
+    VERIFY;
+}
+
+void StackDiscard(Stack* tack){
+    assert(tack);
+    VERIFY;
+
+    for(size_t i = 0; i < tack->maxlen; ++i)
+        *(tack->data + sizeof(CANARY_1) + i * sizeof(element)) = POISON;
+    
+
+    tack->minlen = 0;
+    tack->maxlen = 0;
+    tack->len = 0;
+    tack->hash = 0;
+
+}
+
+element StackPop(Stack* tack){
+    assert(tack);
+    VERIFY;
+
+    tack->len--;
+
+    if(tack->len < tack->minlen)
+        MemShrink(tack);
+    
+
+    element n = *(tack->data + sizeof(CANARY_1) + sizeof(element) * tack->len);
+
+    *(tack->data + sizeof(CANARY_1) + sizeof(element) * tack->len) = 0;
+
+    tack->hash -= n * (tack->len + 1);
+
+    VERIFY;
+
+    return n;
+}
+
+int StackPush(Stack* tack, element n){
+    assert(tack);
+    VERIFY;
+
+    tack->len++;
+
+    if(tack->len > tack->maxlen)
+        MemExpand(tack);
+
+    *(tack->data + sizeof(CANARY_1) + sizeof(element) * (tack->len - 1)) = n;
+
+    tack->hash += *(tack->data + sizeof(CANARY_1) + sizeof(element) * (tack->len - 1)) * tack->len;
+
+    VERIFY;
+
+    return 0;
+}
+
+void MemShrink(Stack* tack){
+    assert(tack);
+    if(tack->minlen == 7)
+        tack->minlen = 0;
+    
+    tack->maxlen = tack->maxlen / 2;
+    tack->minlen = tack->minlen / 2;
+
+    tack->data = (element*) realloc(tack->data, sizeof(CANARY_1) + sizeof(CANARY_2) + tack->maxlen * sizeof(element));
+
+    for(size_t i = tack->len; i < tack->maxlen; ++i){
+        *(tack->data + sizeof(CANARY_1) + i * sizeof(element)) = POISON;
+    }
+
+    if(tack->data == nullptr){
+        StackDump(tack, MEMORY_WASNT_SHRINK);
+        exit(-1);
+    }
+
+    *(tack->data) = CANARY_1;
+    *(tack->data + sizeof(CANARY_1) + tack->maxlen * sizeof(element));
+
+}
+
+void MemExpand(Stack* tack){
+    assert(tack);
+    tack->minlen = (3 * tack->maxlen) / 4;
+    tack->maxlen = tack->maxlen * 2;
+
+    tack->data = (element*) realloc(tack->data, sizeof(CANARY_1) + sizeof(CANARY_2) + tack->maxlen * sizeof(element));
+
+    if((tack->data + sizeof(CANARY_1)) == nullptr){
+        StackDump(tack, MEMORY_WASNT_EXPAND);
+        exit(-1);
+    }
+
+    *(tack->data) = CANARY_1;
+    *(tack->data + sizeof(CANARY_1) + tack->maxlen * sizeof(element)) = CANARY_2;
+
+}
+
+int StackVerify(Stack* tack){
+    assert(tack);
+    if(tack == nullptr)
+        return STACK_STRUCT_NULLPTR;
+
+    if((tack->data + sizeof(CANARY_1)) == nullptr)
+        return STACK_NULLPTR;
+
+    if(tack->minlen > tack->maxlen || tack->minlen < 0 || tack->maxlen < 0 || tack->len < 0)
+        return INVALID_SIZE;
+
+    if(*(tack->data + sizeof(CANARY_1) + tack->maxlen * sizeof(element)) != CANARY_2)
+        return STACK_OVERFLOW;
+
+    if(*(tack->data) != CANARY_1)
+        return STACK_UNDERFLOW;
+
+    size_t sum = 0;
+
+    for(size_t i = 0; i < tack->len; i++)
+        sum += *(tack->data + sizeof(CANARY_1) + sizeof(element) * i) * (i + 1);
+
+    if(sum != tack->hash)
+        return HASH_ERROR;
+
+    return 0;
+}
+
+int StackDump(Stack* tack, int err){
+    assert(tack);
+    FILE* code = fopen("dump.txt", "w");
+
+    fprintf(code, "#####ERROR#####\n");
+
+    fprintf(code, "Upper limit = %zu\n", tack->maxlen);
+
+    fprintf(code, "Lower limit = %d\n", tack->minlen);
+
+    fprintf(code, "Number of elements = %zu\n", tack->len);
+
+    fprintf(code, "Hash = %zu\n", tack->hash);
+
+    fprintf(code, "Adress of start of the Stack = [%p]\n", (tack->data + sizeof(CANARY_1)));
+
+    fprintf(code, "Some elements of Stack: \n");
+
+    if(*(tack->data + sizeof(CANARY_1)) == POISON)
+        fprintf(code, "[1]: POISON\n");
+    else
+        fprintf(code, "[1]: %lg\n", *(tack->data + sizeof(CANARY_1)));
+
+    if(*(tack->data + sizeof(CANARY_1) + sizeof(element)) == POISON)
+        fprintf(code, "[2]: POISON\n");
+    else
+        fprintf(code, "[2]: %lg\n", *(tack->data + sizeof(CANARY_1) + sizeof(element)));
+
+    if(*(tack->data + sizeof(CANARY_1) + 2 * sizeof(element)) == POISON)
+        fprintf(code, "[3]: POISON\n");
+    else
+        fprintf(code, "[3]: %lg\n", *(tack->data + sizeof(CANARY_1) + 2 * sizeof(element) * tack->len));
+
+    if(*(tack->data + sizeof(CANARY_1) + 3 * sizeof(element)) == POISON)
+        fprintf(code, "[4]: POISON\n");
+    else
+        fprintf(code, "[4]: %lg\n", 
+                *(tack->data + sizeof(CANARY_1) + 3 * sizeof(element) * tack->len));
+
+    fprintf(code, ".........\n");
+
+    fprintf(code, "[i]: data[i]\n");
+
+    fprintf(code, ".........\n");
+
+    if(tack->len > 3){
+    
+        fprintf(code, "[%zu]: %lg\n", tack->len, *(tack->data + sizeof(CANARY_1) + sizeof(element) * (tack->len - 1)));
+
+            if(*(tack->data + sizeof(CANARY_1) + sizeof(element) * (tack->len - 1)) == POISON)
+                fprintf(code, "[%zu]: POISON\n", tack->len);
+            else
+                fprintf(code, "[%zu]: %lg\n", tack->len, *(tack->data + sizeof(CANARY_1) + sizeof(element) * (tack->len - 1)));
+    }
+
+    fprintf(code, "What happened: ");
+
+    switch(err){
+
+        case STACK_STRUCT_NULLPTR:  fprintf(code, "STACK STRUCT NULLPOINTER\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return STACK_NULLPTR;
+
+        case STACK_NULLPTR:         fprintf(code, "STACK NULLPOINTER\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return STACK_NULLPTR;
+
+        case INVALID_SIZE:          fprintf(code, "INVALID SIZE\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return INVALID_SIZE;
+
+        case STACK_OVERFLOW:        fprintf(code, "STACK OVERFLOW\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return STACK_OVERFLOW;
+
+        case STACK_UNDERFLOW:       fprintf(code, "STACK UNDERFLOW\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return STACK_UNDERFLOW;
+
+        case HASH_ERROR:            fprintf(code, "HASH ERROR\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return HASH_ERROR;
+
+        case MEMORY_WASNT_SHRINK:   fprintf(code, "MEMORY WASNT SHRINK\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return MEMORY_WASNT_SHRINK;
+
+        case MEMORY_WASNT_EXPAND:   fprintf(code, "MEMORY WASNT EXPAND\n");
+                                    fprintf(code, "CODE ERROR: %d\n", err);
+                                    fclose(code);
+                                    return MEMORY_WASNT_EXPAND;
+
+        default:                    fprintf(code, "NOTHING\n");
+                                    fprintf(code, "THERE IS NO ERROR\n");
+                                    break;
+    }
+
+    fclose(code);
+
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define START 10
+#define CANARY_1 23637
+#define CANARY_2 97654
+#define VERIFY if(StackVerify(tack) != 0){StackDump(tack, StackVerify(tack)); exit(-1);}
+const int POISON = 1357;
+
+typedef double element;
+
+struct Stack{
+    size_t minlen = 0;
+    size_t maxlen = START;
+    size_t len = 0;
+    size_t hash = 0;
+    element* data = NULL;
+};
+
+enum error{
+    STACK_STRUCT_NULLPTR = 10,
+    STACK_NULLPTR = 20,
+    INVALID_SIZE = 30,
+    STACK_OVERFLOW = 40,
+    STACK_UNDERFLOW = 50,
+    MEMORY_WASNT_SHRINK = 60,
+    MEMORY_WASNT_EXPAND = 70,
+    HASH_ERROR = 80
+};
+
+void StackCreate(Stack* tack);
+
+void StackDiscard(Stack* tack);
+
+int StackPush(Stack* tack, element n);
+
+element StackPop(Stack* tack);
+
+void MemExpand(Stack* tack);
+
+void MemShrink(Stack* tack);
+
+int StackVerify(Stack* tack);
+
+int StackDump(Stack* tack, int err);
